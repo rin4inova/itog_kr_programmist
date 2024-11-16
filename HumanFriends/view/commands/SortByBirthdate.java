37e0e9cc1b4e6b36b282cc5b4ada3f746e75d236
Tuyana Rinchinova <rin4inova@gmail.com>
2024-11-16 09:12:48 +0800

    add program HumanFriends

---
+++ HumanFriends/view/commands/SortByBirthdate.java
@@ -0,0 +1,14 @@
+package view.commands;
+import view.View;
+
+public class SortByBirthdate extends Command {
+
+    public SortByBirthdate(View view) {
+        super("Sort by birthdate", view);
+    }
+
+    @Override
+    public void execute(){
+        getView().sortByBirthdate();
+    }
+}
